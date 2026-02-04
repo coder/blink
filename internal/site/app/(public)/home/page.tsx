be35da58314c18f5e70ec8bd1c2cdfb7289b413a
Matt Vollmer <matthewjvollmer@outlook.com>
2026-02-04 13:45:10 +0000

    Increase blink-hop icon to 400x400

--- internal/site/app/(public)/home/page.tsx
+++ internal/site/app/(public)/home/page.tsx
@@ -680,8 +680,8 @@ export default function Home() {
               <Image
                 src="/blink-hop.png"
                 alt="Blink"
-                width={200}
-                height={200}
+                width={400}
+                height={400}
                 className="brightness-0 invert"
               />
             </div>
